400493b5bacd1da54cf6eaba1c40cee7383bb42f
Julian Eggers <eggers.julian@gmail.com>
2018-11-08 22:01:37 +0100

    LIBS-392 - reduce io and startup-time

--- src/main/java/com/avides/springboot/testcontainer/mysql/MysqlProperties.java
+++ src/main/java/com/avides/springboot/testcontainer/mysql/MysqlProperties.java
@@ -26,6 +26,6 @@ public class MysqlProperties extends AbstractEmbeddedContainerProperties
 
     public MysqlProperties()
     {
-        setDockerImage("mysql:5.7.22");
+        setDockerImage("mysql:5.7.24");
     }
 }
